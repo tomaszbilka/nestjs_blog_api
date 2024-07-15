5c90c095699363bd9599d6d267ef215a18a293a2
tomaszbilka <bila4@op.pl>
2024-07-15 16:18:56 +0200

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
-import { TagModule } from '@app/tag/tag.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { TagModule } from '@app/tag/tag.module';
 import ormconfig from '@app/ormconfig';
+import { UserModule } from '@app/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), TagModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '@app/user/dto/createUser.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
+import { Repository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
+import { JWT_SECRET } from '@app/config';
+import { UserResponseInterface } from '@app/user/types/userResponse.interface';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const newUser = new UserEntity();
+    Object.assign(newUser, createUserDto);
+    return await this.userRepository.save(newUser);
+  }
+
+  generateJwt(user: UserEntity): string {
+    return sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      JWT_SECRET,
+    );
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user),
+      },
+    };
+  }
+}
